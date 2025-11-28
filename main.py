08bd2fbcddfb6d1733f48e6e87c3ed916d19822b
the-can-of-soup <campbell+github@campbellkeller.net>
2025-11-28 13:46:50 -0500

    Generalize bruteforcer to n steps

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Callable, Any
+
 # I don't own PowerPoint, so here is the image I used to extract these colors:
 # https://www.empowersuite.com/hs-fs/hubfs/Marketing/Blog/Snips/custom-color-palette-powerpoint-master3.png?width=477&name=custom-color-palette-powerpoint-master3.png
 BASE_COLORS: list[tuple[int, int, int]] = [(0, 51, 102), (51, 102, 153), (51, 102, 204), (0, 51, 153), (0, 0, 153), (0, 0, 204), (0, 0, 102), (0, 102, 102), (0, 102, 153), (0, 153, 204), (0, 102, 204), (0, 51, 204), (0, 0, 255), (51, 51, 255), (51, 51, 153), (0, 128, 128), (0, 153, 153), (51, 204, 204), (0, 204, 255), (0, 153, 255), (0, 102, 255), (51, 102, 255), (51, 51, 204), (102, 102, 153), (51, 153, 102), (0, 204, 153), (0, 255, 204), (0, 255, 255), (51, 204, 255), (51, 153, 255), (102, 153, 255), (102, 102, 255), (102, 0, 255), (102, 0, 204), (51, 153, 51), (0, 204, 102), (0, 255, 153), (102, 255, 204), (102, 255, 255), (102, 204, 255), (153, 204, 255), (153, 153, 255), (153, 102, 255), (153, 51, 255), (153, 0, 255), (0, 102, 0), (0, 204, 0), (0, 255, 0), (102, 255, 153), (153, 255, 204), (204, 255, 255), (204, 236, 255), (204, 204, 255), (204, 153, 255), (204, 102, 255), (204, 0, 255), (153, 0, 204), (0, 51, 0), (0, 128, 0), (51, 204, 51), (102, 255, 102), (153, 255, 153), (204, 255, 204), (255, 255, 255), (255, 204, 255), (255, 153, 255), (255, 102, 255), (255, 0, 255), (204, 0, 204), (102, 0, 102), (51, 102, 0), (0, 153, 0), (102, 255, 51), (153, 255, 102), (204, 255, 153), (255, 255, 204), (255, 204, 204), (255, 153, 204), (255, 102, 204), (255, 51, 204), (204, 0, 153), (128, 0, 128), (51, 51, 0), (102, 153, 0), (153, 255, 51), (204, 255, 102), (255, 255, 153), (255, 204, 153), (255, 153, 153), (255, 102, 153), (255, 51, 153), (204, 51, 153), (153, 0, 153), (102, 102, 51), (153, 204, 0), (204, 255, 51), (255, 255, 102), (255, 204, 102), (255, 153, 102), (255, 124, 128), (255, 0, 102), (214, 0, 147), (153, 51, 102), (128, 128, 0), (204, 204, 0), (255, 255, 0), (255, 204, 0), (255, 153, 51), (255, 102, 0), (255, 80, 80), (204, 0, 102), (102, 0, 51), (153, 102, 51), (204, 153, 0), (255, 153, 0), (204, 102, 0), (255, 51, 0), (255, 0, 0), (204, 0, 0), (153, 0, 51), (102, 51, 0), (153, 102, 0), (204, 51, 0), (153, 51, 0), (153, 0, 0), (128, 0, 0), (165, 0, 33), (7, 7, 8), (16, 16, 16), (28, 28, 28), (41, 41, 41), (51, 51, 51), (77, 77, 77), (95, 95, 95), (119, 119, 119), (128, 128, 128), (150, 150, 150), (178, 178, 178), (192, 192, 192), (221, 221, 221), (234, 234, 234), (248, 248, 248)]
@@ -99,17 +101,42 @@ class Solution:
 
         return result
 
-def get_constructible_colors_from_2_steps() -> set[int]:
+def get_constructible_colors_from_n_steps(n: int = 2) -> set[int]:
     constructible_colors: set[int] = set()
-    for color1 in range(len(BASE_COLORS)):
-        for opacity in range(len(BASE_OPACITIES)):
-            for color2 in range(len(BASE_COLORS)):
-                solution = Solution([(color1, FULLY_OPAQUE_INDEX), (color2, opacity)])
-                constructible_colors.add(rgb_to_decimal(solution.test()))
+    colors_range: range = range(len(BASE_COLORS))
+    opacities_range: range = range(len(BASE_OPACITIES))
+
+    def for_every_solution(step_count: int, function: Callable[[list[tuple[int, int]]], Any], steps: list[tuple[int, int]] | None = None) -> None:
+        if steps is None:
+            steps = []
+        if step_count < 1:
+            function(steps)
+            return
+        if step_count == 1:
+            for color in colors_range:
+                new_steps: list[tuple[int, int]] = steps.copy()
+                new_steps.append((color, FULLY_OPAQUE_INDEX))
+                for_every_solution(step_count - 1, function, new_steps)
+        else:
+            for color in colors_range:
+                for opacity in opacities_range:
+                    new_steps: list[tuple[int, int]] = steps.copy()
+                    new_steps.append((color, opacity))
+                    for_every_solution(step_count - 1, function, new_steps)
+
+    def add_result_to_constructible_colors(steps: list[tuple[int, int]]) -> None:
+        nonlocal constructible_colors
+
+        solution = Solution(steps)
+        constructible_colors.add(rgb_to_decimal(solution.test()))
+
+    for step_count in range(1, n + 1):
+        for_every_solution(step_count, add_result_to_constructible_colors)
+
     return constructible_colors
 
 if __name__ == '__main__':
     print('Getting constructible colors...')
-    constructible_colors: set[int] = get_constructible_colors_from_2_steps()
+    constructible_colors: set[int] = get_constructible_colors_from_n_steps(2)
     print(f'{len(constructible_colors)} / {1 << 24} colors constructible.')
     print(f'{(1 << 24) - len(constructible_colors)} / {1 << 24} colors unconstructible.')
