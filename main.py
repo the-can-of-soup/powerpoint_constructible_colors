fb28689857e8b9d42a64548d0a47fd9a0d9e3daf
the-can-of-soup <campbell+github@campbellkeller.net>
2025-11-28 16:34:00 -0500

    Fix progress bars in PyPy

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 from typing import Callable, Any
 from tqdm import tqdm # pip install tqdm
+import platform
 
 # I don't own PowerPoint, so here is the image I used to extract these colors:
 # https://www.empowersuite.com/hs-fs/hubfs/Marketing/Blog/Snips/custom-color-palette-powerpoint-master3.png?width=477&name=custom-color-palette-powerpoint-master3.png
 BASE_COLORS: list[tuple[int, int, int]] = [(0, 51, 102), (51, 102, 153), (51, 102, 204), (0, 51, 153), (0, 0, 153), (0, 0, 204), (0, 0, 102), (0, 102, 102), (0, 102, 153), (0, 153, 204), (0, 102, 204), (0, 51, 204), (0, 0, 255), (51, 51, 255), (51, 51, 153), (0, 128, 128), (0, 153, 153), (51, 204, 204), (0, 204, 255), (0, 153, 255), (0, 102, 255), (51, 102, 255), (51, 51, 204), (102, 102, 153), (51, 153, 102), (0, 204, 153), (0, 255, 204), (0, 255, 255), (51, 204, 255), (51, 153, 255), (102, 153, 255), (102, 102, 255), (102, 0, 255), (102, 0, 204), (51, 153, 51), (0, 204, 102), (0, 255, 153), (102, 255, 204), (102, 255, 255), (102, 204, 255), (153, 204, 255), (153, 153, 255), (153, 102, 255), (153, 51, 255), (153, 0, 255), (0, 102, 0), (0, 204, 0), (0, 255, 0), (102, 255, 153), (153, 255, 204), (204, 255, 255), (204, 236, 255), (204, 204, 255), (204, 153, 255), (204, 102, 255), (204, 0, 255), (153, 0, 204), (0, 51, 0), (0, 128, 0), (51, 204, 51), (102, 255, 102), (153, 255, 153), (204, 255, 204), (255, 255, 255), (255, 204, 255), (255, 153, 255), (255, 102, 255), (255, 0, 255), (204, 0, 204), (102, 0, 102), (51, 102, 0), (0, 153, 0), (102, 255, 51), (153, 255, 102), (204, 255, 153), (255, 255, 204), (255, 204, 204), (255, 153, 204), (255, 102, 204), (255, 51, 204), (204, 0, 153), (128, 0, 128), (51, 51, 0), (102, 153, 0), (153, 255, 51), (204, 255, 102), (255, 255, 153), (255, 204, 153), (255, 153, 153), (255, 102, 153), (255, 51, 153), (204, 51, 153), (153, 0, 153), (102, 102, 51), (153, 204, 0), (204, 255, 51), (255, 255, 102), (255, 204, 102), (255, 153, 102), (255, 124, 128), (255, 0, 102), (214, 0, 147), (153, 51, 102), (128, 128, 0), (204, 204, 0), (255, 255, 0), (255, 204, 0), (255, 153, 51), (255, 102, 0), (255, 80, 80), (204, 0, 102), (102, 0, 51), (153, 102, 51), (204, 153, 0), (255, 153, 0), (204, 102, 0), (255, 51, 0), (255, 0, 0), (204, 0, 0), (153, 0, 51), (102, 51, 0), (153, 102, 0), (204, 51, 0), (153, 51, 0), (153, 0, 0), (128, 0, 0), (165, 0, 33), (7, 7, 8), (16, 16, 16), (28, 28, 28), (41, 41, 41), (51, 51, 51), (77, 77, 77), (95, 95, 95), (119, 119, 119), (128, 128, 128), (150, 150, 150), (178, 178, 178), (192, 192, 192), (221, 221, 221), (234, 234, 234), (248, 248, 248)]
 BASE_OPACITIES: list[float] = [0.05, 0.2, 0.35, 0.5, 0.7, 0.85, 1]
 FULLY_OPAQUE_INDEX: int = 6
+PY_IMPLEMENTATION: str = platform.python_implementation()
 
 def rgb_to_hex(rgb: tuple[int, int, int]) -> str:
     return f'#{hex(rgb[0])[2:]}{hex(rgb[1])[2:]}{hex(rgb[2])[2:]}'
@@ -126,8 +128,8 @@ def get_constructible_colors_from_n_steps(n: int = 2) -> set[int]:
         partial_combinations *= len(BASE_COLORS)
         total_combinations += partial_combinations
     total_combinations: int = int(total_combinations)
-    progress_bar: tqdm = tqdm(desc='Progress     ', total=total_combinations)
-    constructible_bar: tqdm = tqdm(desc='Constructible', total=(1 << 24))
+    progress_bar: tqdm = tqdm(desc='Progress     ', total=total_combinations, ascii=(PY_IMPLEMENTATION == 'PyPy'))
+    constructible_bar: tqdm = tqdm(desc='Constructible', total=(1 << 24), ascii=(PY_IMPLEMENTATION == 'PyPy'))
 
     def for_every_solution(step_count: int, function: Callable[[list[tuple[int, int]]], Any], steps: list[tuple[int, int]] | None = None) -> None:
         if steps is None:
