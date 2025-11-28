0c69ac2ab21543772f6ae6342d5022aee8164253
the-can-of-soup <campbell+github@campbellkeller.net>
2025-11-28 13:58:06 -0500

    Add progress bar to bruteforcer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import Callable, Any
+from tqdm import tqdm # pip install tqdm
 
 # I don't own PowerPoint, so here is the image I used to extract these colors:
 # https://www.empowersuite.com/hs-fs/hubfs/Marketing/Blog/Snips/custom-color-palette-powerpoint-master3.png?width=477&name=custom-color-palette-powerpoint-master3.png
@@ -106,7 +107,16 @@ def get_constructible_colors_from_n_steps(n: int = 2) -> set[int]:
     colors_range: range = range(len(BASE_COLORS))
     opacities_range: range = range(len(BASE_OPACITIES))
 
-    def for_every_solution(step_count: int, function: Callable[[list[tuple[int, int]]], Any], steps: list[tuple[int, int]] | None = None) -> None:
+    total_combinations: float = 0
+    for step_count in range(1, n + 1):
+        partial_combinations = (len(BASE_COLORS) * len(BASE_OPACITIES)) ** (step_count - 1)
+        partial_combinations *= len(BASE_COLORS)
+        total_combinations += partial_combinations
+    total_combinations: int = int(total_combinations)
+    progress_bar: tqdm = tqdm(total=total_combinations)
+
+    def for_every_solution(step_count: int, function: Callable[[list[tuple[int, int]]], Any], steps: list[tuple[int, int]] | None = None, progress: int = 0) -> None:
+        progress_bar.update(progress)
         if steps is None:
             steps = []
         if step_count < 1:
@@ -118,27 +128,28 @@ def get_constructible_colors_from_n_steps(n: int = 2) -> set[int]:
             for color in colors_range:
                 new_steps: list[tuple[int, int]] = steps.copy()
                 new_steps.append((color, FULLY_OPAQUE_INDEX))
-                for_every_solution(step_count - 1, function, new_steps)
+                for_every_solution(step_count - 1, function, new_steps, progress + 1)
         else:
             for color in colors_range:
                 for opacity in opacities_range:
                     new_steps: list[tuple[int, int]] = steps.copy()
                     new_steps.append((color, opacity))
-                    for_every_solution(step_count - 1, function, new_steps)
+                    for_every_solution(step_count - 1, function, new_steps, progress + 1)
 
     def add_result_to_constructible_colors(steps: list[tuple[int, int]]) -> None:
         nonlocal constructible_colors
 
-        solution = Solution(steps)
+        solution: Solution = Solution(steps)
         constructible_colors.add(rgb_to_decimal(solution.test()))
 
     for step_count in range(1, n + 1):
         for_every_solution(step_count, add_result_to_constructible_colors)
+    progress_bar.close()
 
     return constructible_colors
 
 if __name__ == '__main__':
     print('Getting constructible colors...')
-    constructible_colors: set[int] = get_constructible_colors_from_n_steps(2)
+    constructible_colors: set[int] = get_constructible_colors_from_n_steps(3)
     print(f'{len(constructible_colors)} / {1 << 24} colors constructible.')
     print(f'{(1 << 24) - len(constructible_colors)} / {1 << 24} colors unconstructible.')
